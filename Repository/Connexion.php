011a0af6bd50cba12707b79af034275734accebf
b22013685 <rayane.belabbas@etu.univ-amu.com>
2023-10-02 10:38:20 +0200

    singleton Connexion.php

--- Repository/Connexion.php
+++ Repository/Connexion.php
@@ -2,5 +2,13 @@
 
 class Connexion
 {
+    private static ?PDO $instance;
 
+    public static function getInstance(): PDO
+    {
+        if (self::$instance == null) {
+            self::$instance = new \PDO(getenv('DB_DND_DSN'), getenv('DB_DND_USER'), getenv('DB_DND_PASSWORD'));
+        }
+        return self::$instance;
+    }
 }
