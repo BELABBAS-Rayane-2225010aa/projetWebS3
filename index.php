a52b4ea06c7fe5a3db4d5f3d88931fedbdd0acf7
AxelROUBAUDR22024628 <axel.roubaud@etu.univ-amu.fr>
2023-10-02 11:20:47 +0200

    mise a jour bouton en flex box

--- index.php
+++ index.php
@@ -5,26 +5,39 @@ start_page('Acceuil');
 <body>
 <?php $active = 'index';
 require 'View/Page/headerMenu.php' ?>
-<button name="Billet1" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
-    Titre1 text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text
-    auteur1
-</button>
-<button name="Billet2" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
-    Titre2
-    auteur2
-</button>
-<button name="Billet3" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
-    Titre3
-    auteur3
-</button>
-<button name="Billet4" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
-    Titre4
-    auteur4
-</button>
-<button name="Billet5" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
-    Titre5
-    auteur5
-</button>
+<section class="section-flex">
+    <button class="btn-flex" onclick="window.location.href='Modules/Pages/Billet.php';">
+      <span class="icone-btn">
+      </span>
+        <p class="txt-btn">Block Testtesttetstestest</p>
+    </button>
+
+    <button class="btn-flex" onclick="window.location.href='Modules/Pages/Billet.php';">
+      <span class="icone-btn">
+      </span>
+        <p class="txt-btn">Block 2</p>
+    </button>
+
+    <button class="btn-flex" onclick="window.location.href='Modules/Pages/Billet.php';">
+      <span class="icone-btn">
+      </span>
+        <p class="txt-btn">Block 3</p>
+    </button>
+
+    <button class="btn-flex" onclick="window.location.href='Modules/Pages/Billet.php';">
+      <span class="icone-btn">
+      </span>
+        <p class="txt-btn">Block 4</p>
+    </button>
+
+    <button class="btn-flex" onclick="window.location.href='Modules/Pages/Billet.php';">
+      <span class="icone-btn">
+      </span>
+        <p class="txt-btn">Block 5</p>
+    </button>
+
+
+</section>
 <?php require 'View/Page/endpage.php' ?>
 <?php
 end_page();
