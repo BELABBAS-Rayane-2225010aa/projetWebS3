9f85f4223a72924f0815abadfee299d4cfec9bcb
AxelROUBAUDR22024628 <axel.roubaud@etu.univ-amu.fr>
2023-10-02 11:09:01 +0200

    mise a jour arborecense billet

--- index.php
+++ index.php
@@ -5,23 +5,23 @@ start_page('Acceuil');
 <body>
 <?php $active = 'index';
 require 'View/Page/headerMenu.php' ?>
-<button name="Billet1" type="button" class="boutonBillet" onclick="window.location.href='Modules/Pages/Billet.php';">
+<button name="Billet1" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
     Titre1 text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text
     auteur1
 </button>
-<button name="Billet2" type="button" class="boutonBillet" onclick="window.location.href='Modules/Pages/Billet.php';">
+<button name="Billet2" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
     Titre2
     auteur2
 </button>
-<button name="Billet3" type="button" class="boutonBillet" onclick="window.location.href='Modules/Pages/Billet.php';">
+<button name="Billet3" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
     Titre3
     auteur3
 </button>
-<button name="Billet4" type="button" class="boutonBillet" onclick="window.location.href='Modules/Pages/Billet.php';">
+<button name="Billet4" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
     Titre4
     auteur4
 </button>
-<button name="Billet5" type="button" class="boutonBillet" onclick="window.location.href='Modules/Pages/Billet.php';">
+<button name="Billet5" type="button" class="boutonBillet" onclick="window.location.href='View/Page/Billet.php';">
     Titre5
     auteur5
 </button>
