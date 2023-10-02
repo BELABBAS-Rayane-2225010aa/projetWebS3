7c6feee9a555a1d81019ae504eb4e13954bf90b4
r22021804 <antonin.rousset@etu.univ-amu.fr>
2023-10-02 11:13:42 +0200

    + fix favicon

--- View/Page/startpage.php
+++ View/Page/startpage.php
@@ -5,7 +5,7 @@ function start_page($title): void
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link rel="icon" href="../../View/Image/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="../../View/image/favicon.ico" type="image/x-icon" />
     <title><?php echo $title; ?></title>
     <link id="theme" rel="stylesheet" href="../../View/Style/style.css" media="all and (orientation: landscape)">
     <link id="themeP" rel="stylesheet" href="../../View/Styles/stylePortable.css" media="all and (orientation: portrait)">
