f81de76aa1f9519e4845d40a396afa138b190ab4
r22021804 <antonin.rousset@etu.univ-amu.fr>
2023-10-06 12:12:11 +0200

    + 404 page

--- View/404.php
+++ View/404.php
@@ -1,13 +1,13 @@
 <?php require 'startpage.php' ?>
-<?php
-start_page('404');
-?>
+<?php start_page('404'); ?>
 <body><!-- FLEX BLOCK-->
-<?php $active = '404';
-require 'headerMenu.php' ?>
-
-<h1>404 Page introuvable!</h1>
-<h2>Vous vous êtes perdu !</h2>
+    <?php $active = '404';
+    require 'headerMenu.php' ?>
+    <section class="section404">
+        <h1>404 Page introuvable!</h1>
+        <h2>Vous vous êtes perdu !</h2>
+        <img class="image404" src="https://th.bing.com/th/id/OIG.4DKj3q0Y6tlL0zjwrZSq?pid=ImgGn">
+    </section>
 <?php require 'endpage.php' ?>
 <?php
 end_page();
--- View/changermdp.php
+++ View/changermdp.php
@@ -3,19 +3,19 @@
 start_page('Changer mot de passe');
 ?>
 <body><!-- FLEX BLOCK-->
-<?php $active = 'mon compte';
-require 'headerMenu.php' ?>
-<section id="changermdp" class="changer_mdp">
-    <p>Entrez votre mot de passe :</p>
-    <input type="text" id="in">
-    <p>Nouveau mot de passe :</p>
-    <input type="text" id="in">
-    <p>Confirmer nouveau mot de passe :</p>
-    <input type="text" id="in">
-    <div></div>
-    <button id='boutonchangerMDP' class='boutonchanger_mdp' onclick="window.location.href='changermdp.php';">changer mot de passe</button>
+    <?php $active = 'mon compte';
+    require 'headerMenu.php' ?>
+    <section id="changermdp" class="changer_mdp">
+        <p>Entrez votre mot de passe :</p>
+        <input type="text" id="in">
+        <p>Nouveau mot de passe :</p>
+        <input type="text" id="in">
+        <p>Confirmer nouveau mot de passe :</p>
+        <input type="text" id="in">
+        <div></div>
+        <button id='boutonchangerMDP' class='boutonchanger_mdp' onclick="window.location.href='changermdp.php';">changer mot de passe</button>
 
-</section>
+    </section>
 <?php require 'endpage.php' ?>
 <?php
 end_page();
