2238b672a29f807d34fb74c2d00ca89f51809450
r22021804 <antonin.rousset@etu.univ-amu.fr>
2023-10-06 12:12:11 +0200

    + 404 page

--- View/categorie.php
+++ View/categorie.php
@@ -2,8 +2,8 @@
 <?php
 start_page('Categorie');
 ?>
-<?php $active = 'categorie';
-require 'headerMenu.php' ?>
+    <?php $active = 'categorie';
+    require 'headerMenu.php' ?>
 <?php require 'endpage.php' ?>
 <?php
 end_page();
