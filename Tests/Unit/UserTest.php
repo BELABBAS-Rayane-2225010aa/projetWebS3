0b6ba274f9b1f691030dc50bf119159e42a4968c
Enzo <enzo.hourlay@etu.univ-amu.fr>
2023-10-06 10:47:14 +0200

    feat: ajout tests des getters de User

--- Tests/Unit/UserTest.php
+++ Tests/Unit/UserTest.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once './Model/User.php';
+
 use Model\User;
 use PHPUnit\Framework\TestCase;
 
@@ -20,5 +23,60 @@ class UserTest extends TestCase
         $expected = "motdepasse";
         //alors(then) on vérifie que password a été renvoyer
         $this->assertSame($expected,$user->getPassword());
+    }
+
+    /** @test */
+    public function test_GetImgPath(): void
+    {
+        //etant donné que(given) un nouvel user
+        $user = new User("motdepasse","","TestPseudo","email","10/08","10/08");
+        //quand(when) on appel la méthode getPassword
+        $expected = "";
+        //alors(then) on vérifie que password a été renvoyer
+        $this->assertSame($expected,$user->getImgPath());
+    }
+
+    /** @test */
+    public function test_GetPseudo(): void
+    {
+        //etant donné que(given) un nouvel user
+        $user = new User("motdepasse","","TestPseudo","email","10/08","10/08");
+        //quand(when) on appel la méthode getPassword
+        $expected = "TestPseudo";
+        //alors(then) on vérifie que password a été renvoyer
+        $this->assertSame($expected,$user->getPseudo());
+    }
+
+    /** @test */
+    public function test_GetEmail(): void
+    {
+        //etant donné que(given) un nouvel user
+        $user = new User("motdepasse","","TestPseudo","email","10/08","10/08");
+        //quand(when) on appel la méthode getPassword
+        $expected = "email";
+        //alors(then) on vérifie que password a été renvoyer
+        $this->assertSame($expected,$user->getEmail());
+    }
+
+    /** @test */
+    public function test_GetDateFirstCo(): void
+    {
+        //etant donné que(given) un nouvel user
+        $user = new User("motdepasse","","TestPseudo","email","10/08","10/08");
+        //quand(when) on appel la méthode getPassword
+        $expected = "10/08";
+        //alors(then) on vérifie que password a été renvoyer
+        $this->assertSame($expected,$user->getDateFirstCo());
+    }
+
+    /** @test */
+    public function test_GetDateLastCo(): void
+    {
+        //etant donné que(given) un nouvel user
+        $user = new User("motdepasse","","TestPseudo","email","10/08","12/06");
+        //quand(when) on appel la méthode getPassword
+        $expected = "12/06";
+        //alors(then) on vérifie que password a été renvoyer
+        $this->assertSame($expected,$user->getDateLastCo());
     }
 }
