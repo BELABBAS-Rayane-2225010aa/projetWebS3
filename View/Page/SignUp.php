a29b0c5588652e250ced1c27099bdbe15d342a10
Crespin Alexandre <kunail0804@gmail.com>
2023-10-08 17:12:06 +0200

    page de formulaire d'inscription

--- View/Page/SignUp.php
+++ View/Page/SignUp.php
@@ -2,7 +2,7 @@
     <strong> INSCRIPTION </strong>
     <br>
     <label>Pseudo :</label>
-    <input name="Pseudo" id="Pseudo" type="text" />
+    <input name="pseudo" id="Pseudo" type="text" />
 
     <label>e-mail :</label>
     <input name="email" id="email" type="email" />
@@ -11,13 +11,13 @@
     <input name="email1" id="email1" type="email" />
 
     <label>Mot de passe :</label>
-    <input name="mdp" id="mdp" type="password" />
+    <input name="$password" id="$password" type="password" />
 
     <label>Confirmez votre mot de passe :</label>
-    <input name="mdp1" id="mdp1" type="password" /><br>
+    <input name="$password1" id="$password1" type="password" /><br>
 
     <label>Une image (optionnel)</label><br>
-    <input name="image" id="image" type="file" /></p>
+    <input name="imgPath" id="image" type="file" /></p>
 
     <button type="submit">Valider</button>
 </form>
