6f5024ba069c153961ab0cdcd190eca9485c4aac
Crespin Alexandre <kunail0804@gmail.com>
2023-10-08 17:11:20 +0200

    passage de get a post

--- View/Page/Login.php
+++ View/Page/Login.php
@@ -1,5 +1,5 @@
-<form action="" method="get">
-    Login<input name="login" type="text"><br>
+<form action="" method="post">
+    Login<input name="pseudo" type="text"><br>
     Mot de passe <input type="password" name="password"><br>
     <input type="submit"  name="action">
 
