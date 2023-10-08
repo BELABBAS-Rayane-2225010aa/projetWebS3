d3d03a2bfe199b9ce3e0ee42ea3e0137d93495c7
Crespin Alexandre <kunail0804@gmail.com>
2023-10-08 15:20:32 +0200

    modification de nom et ajout du fichier View pour le formulaire d'inscription et le controller associer

---
+++ View/Page/SignUp.php
@@ -0,0 +1,23 @@
+<form action="" method="post">
+    <strong> INSCRIPTION </strong>
+    <br>
+    <label>Pseudo :</label>
+    <input name="Pseudo" id="Pseudo" type="text" />
+
+    <label>e-mail :</label>
+    <input name="email" id="email" type="email" />
+
+    <label>Confirmer votre e-mail :</label>
+    <input name="email1" id="email1" type="email" />
+
+    <label>Mot de passe :</label>
+    <input name="mdp" id="mdp" type="password" />
+
+    <label>Confirmez votre mot de passe :</label>
+    <input name="mdp1" id="mdp1" type="password" /><br>
+
+    <label>Une image (optionnel)</label><br>
+    <input name="image" id="image" type="file" /></p>
+
+    <button type="submit">Valider</button>
+</form>
