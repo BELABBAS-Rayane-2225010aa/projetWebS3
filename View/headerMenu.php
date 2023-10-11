e2aa18cb37ed716f01d8d4a2418bebbaa4f3b1ad
AxelROUBAUDR22024628 <axel.roubaud@etu.univ-amu.fr>
2023-10-11 16:05:58 +0200

    mis a jour du menu bar avec effet de flou et changement de couleur

--- View/headerMenu.php
+++ View/headerMenu.php
@@ -1,8 +1,17 @@
-<ul class="menu"> <!--Menu de haut de page qui est prèsent sur toute les page-->
+<ul class="menu"><!--Menu de haut de page qui est prèsent sur toute les page-->
+    <div class="blur"></div>
     <li><a <?php if ($active === 'index'){echo 'class=active';};?> href="../index.php">Acceuil</a></li>
-    <li><a <?php if ($active === 'categorie'){echo 'class=active';};?> href="/View/categorie.php">Categorie</a></li>
+    <li class="dropdown" >
+        <a  class="dropbtn"  <?php if ($active === 'categorie'){echo 'class=active';};?> href="/View/categorie.php">Categorie</a>
+        <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+        </div>
+    </li>
     <li><a <?php if ($active === 'contact'){echo 'class=active';};?> href="/View/contact.php">Contact</a></li>
     <li style="float:right"><a <?php if ($active === 'about'){echo 'class=active';}?> href="/View/about.php">About</a></li>
     <li style="float:right"><a <?php if ($active === 'connection'){echo 'class=active';}?> href="/View/about.php">Se connecter</a></li>
     <li style="float:right"><a <?php if ($active === 'cree_compte'){echo 'class=active';}?> href="/View/about.php">Cree un compte</a></li>
 </ul>
+
