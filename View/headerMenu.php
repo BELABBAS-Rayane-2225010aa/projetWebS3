cddb78053efe8284fbbe2200afd6aff7036026a5
Crespin Alexandre <kunail0804@gmail.com>
2023-10-11 10:29:16 +0200

    fix de l'header menu qui renvoyé pas vers les pages

--- View/headerMenu.php
+++ View/headerMenu.php
@@ -3,6 +3,6 @@
     <li><a <?php if ($active === 'categorie'){echo 'class=active';};?> href="/View/categorie.php">Categorie</a></li>
     <li><a <?php if ($active === 'contact'){echo 'class=active';};?> href="/View/contact.php">Contact</a></li>
     <li style="float:right"><a <?php if ($active === 'about'){echo 'class=active';}?> href="/View/about.php">About</a></li>
-    <li style="float:right"><a <?php if ($active === 'connection'){echo 'class=active';}?> href="/View/about.php">Se connecter</a></li>
-    <li style="float:right"><a <?php if ($active === 'cree_compte'){echo 'class=active';}?> href="/View/about.php">Cree un compte</a></li>
+    <li style="float:right"><a <?php if ($active === 'connection'){echo 'class=active';}?> href="/View/Login.php">Se connecter</a></li>
+    <li style="float:right"><a <?php if ($active === 'cree_compte'){echo 'class=active';}?> href="/View/SignUp.php">Cree un compte</a></li>
 </ul>
