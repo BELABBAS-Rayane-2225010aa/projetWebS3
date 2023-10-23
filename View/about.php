2016df88f5f2268d5d9171a16ae5cfe0274b7b3c
r22021804 <antonin.rousset@etu.univ-amu.fr>
2023-10-23 14:10:05 +0200

    + about

---
+++ View/about.php
@@ -0,0 +1,19 @@
+<?php
+require '../vendor/autoload.php';
+require 'GestionPage.php' ?>
+<?php
+start_page('Billet');//Charge la balise "head" avec le css, favicon et le nom de la page donner en parametre.
+?>
+<body>
+<?php $active = 'Billet';
+require 'HeaderMenu.php';//Charge le bar menu?>
+<section class="fomBox">
+    <label>
+        Ce site internet est réalisé pour un travail de groupe d'étudiant.<br>
+        Ce site est libre de droit.<br>
+        Réaliser par: Belabbas,Créspin,De Angeli,Hourlay,Roubaud,Rousset
+    </label><br>
+</section>
+<?php
+end_page();
+?>
