7fd501e6576a695e3a98ec476e90fe1a368105f8
Crespin Alexandre <kunail0804@gmail.com>
2023-10-24 09:55:59 +0200

    fix form

--- View/Home.php
+++ View/Home.php
@@ -19,33 +19,35 @@ $active = 'index';
 require 'HeaderMenu.php';//Charge le bar menu
 ?>
     <section class="section-flex"><!--affiche les 5 dernier billet créé-->
-        <form action="Billet.php" method="post" id="billetform"></form>
-        <?php
-        for ($i = 0 ; $i < 5 ; ++$i)
-        {
-        ?>
-            <button class="btn-flex" value="<?php echo base64_encode(serialize($cinqBillet[$i]));?>" name="billetClique" form="billetform">
-                <span class="icone-btn">
-                </span>
-                    <p class="txt-btn"><?php if (isset($cinqBillet[$i])){echo $cinqBillet[$i]->getTitle();}else{ echo 'erreur de chargement du billet';}?></p>
-            </button>
-        <?php
-        }
-        ?>
+        <form action="Billet.php" method="post" id="billetform">
+            <?php
+            for ($i = 0 ; $i < 5 ; ++$i)
+            {
+            ?>
+                <button class="btn-flex" value="<?php echo base64_encode(serialize($cinqBillet[$i]));?>" name="billetClique" form="billetform">
+                    <span class="icone-btn">
+                    </span>
+                        <p class="txt-btn"><?php if (isset($cinqBillet[$i])){echo $cinqBillet[$i]->getTitle();}else{ echo 'erreur de chargement du billet';}?></p>
+                </button>
+            <?php
+            }
+            ?>
+        </form>
     </section>
 
     <section>
-        <form class="fomBox" id="connectedform">affiche tous les billet</form>
-        <?php
-        if (isset($connectedArray)){
-            for ($i = 0 ; $i < sizeof($connectedArray) ; ++$i){
-        ?>
-                <button value="<?php echo base64_encode(serialize($connectedArray[$i]));?>" name="userClique" form="connectedform">
-                    <?php echo $connectedArray[$i]->getPseudo();
+        <form class="fomBox" id="connectedform">
+            <?php
+            if (isset($connectedArray)){
+                for ($i = 0 ; $i < sizeof($connectedArray) ; ++$i){
+            ?>
+                    <button value="<?php echo base64_encode(serialize($connectedArray[$i]));?>" name="userClique" form="connectedform">
+                        <?php echo $connectedArray[$i]->getPseudo();
 
+                }
             }
-        }
-        ?>
+            ?>
+        </form>
     </section>
 
 <?php
