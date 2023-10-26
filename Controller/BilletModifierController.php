339028432b5311c5a0b2e587b87bf3f13a0d6a07
Crespin Alexandre <kunail0804@gmail.com>
2023-10-26 15:42:20 +0200

    feat : mise en place de la possibilité de modifier un billet

--- Controller/BilletModifierController.php
+++ Controller/BilletModifierController.php
@@ -7,40 +7,19 @@ use App\Repository\BilletRepository;
 
 class BilletModifierController
 {
-    private array $billetArray;
-    public function BilletArrayPrivate() : void {
-        $authorId = $_SESSION['user']->getUserId();
-        try {
-            $repo = new BilletRepository();
-            $this->billetArray = $repo->getBilletArrayByAuthor($authorId);
+    public function updateBillet(): void {
+        $oldTitle = $_POST['oldTitle'];
+        $title = $_POST['title'];
+        $msg = $_POST['desc'];
+        $dateBillet = date("Y-m-d H:i:s");
+        $authorId = $_POST['authorID'];
+        $categoryId = $_POST['category'];
+        try{
+            $billet = new BilletRepository();
+            $billet->updateBillet($oldTitle,$title,$msg,$dateBillet,$authorId,$categoryId);
         }
         catch(CannotFindBilletException $ERROR){
-            $this->billetArray = [];
+            file_put_contents('Log/[PlaceHolderName].log', $ERROR->getMessage()."\n",FILE_APPEND | LOCK_EX);
         }
-    }
-
-    public function BilletArrayPublic() : void {
-        if (isset($_POST['userClique'])){
-            $serializedUser = $_POST['userClique'];
-            $userClique = unserialize(base64_decode($serializedUser));
-            $authorId = $userClique->getUserId();
-        }
-
-        //ATTENTION ceci est a mesure de test
-        else {
-            $authorId = 1;
-        }
-
-        try {
-            $repo = new BilletRepository();
-            $this->billetArray = $repo->getBilletArrayByAuthor($authorId);
-        }
-        catch(CannotFindBilletException $ERROR){
-            $this->billetArray = [];
-        }
-    }
-
-    public function getBilletArray() : array{
-        return $this->billetArray;
     }
 }
--- View/BilletModifier.php
+++ View/BilletModifier.php
@@ -28,28 +28,39 @@ $arrayCat = $categoryRepository->getCat();
 ?>
 <script src="Bouton.js"></script>
     <button onclick="BoutonAffichageCategorie()" class="buttonCategory"><img src="../View/image/BtnCategorie.ico" /></button><!--Présent qu'en mode portable sert a ameliorer la lisibiliter du billet-->
-    <form method="post" action="../index.php">
-        <aside id="CategorieID" class="category"><!--affiche les categories du billet-->
-            <h2>Categories ID : <?php echo $billetClique->getCategoryId() ?></h2>
-            <select id="category" name="category">
-                <?php
-                for ($i = 0 ; $i < sizeof($arrayCat) ; ++$i)
-                {
-                    ?>
-                    <option value="<?php echo $arrayCat[$i]["CAT_ID"] ?>"><?php echo $arrayCat[$i]["LABEL"] ?></option>
-                    <?php
-                }
+<form method="post" action="../index.php">
+    <aside id="CategorieID" class="category"><!--affiche les categories du billet-->
+        <h2>Categories ID : <?php echo $billetClique->getCategoryId() ?></h2>
+        <select id="category" name="category">
+            <?php
+            for ($i = 0 ; $i < sizeof($arrayCat) ; ++$i)
+            {
                 ?>
-            </select>
-        </aside>
+                <option value="<?php echo $arrayCat[$i]["CAT_ID"] ?>"><?php echo $arrayCat[$i]["LABEL"] ?></option>
+                <?php
+            }
+            ?>
+        </select>
+    </aside>
 
-        <section id="commentID" class="comments"><!--affiche les comentaires du billet-->
-            <h2><?php echo $billetClique->getTitle()?></h2>
-            <p><?php if(!empty($billetClique->getMsg())){echo $billetClique->getMsg();}else{echo 'ERROR Le message est vide ERROR';}?></p>
-            <h3>AuteurID : <?php echo $billetClique->getAuthorId() ?></h3>
-            <p>Date : <?php echo $billetClique->getDate() ?></p>
-        </section>
-    </form>
+    <section id="commentID" class="comments"><!--affiche les comentaires du billet-->
+        <h2>
+            <label>Titre :
+                <input type="text" name="title" value="<?php echo $billetClique->getTitle()?>"/>
+            </label>
+        </h2>
+        <label>description :
+            <textarea name="desc"><?php if(!empty($billetClique->getMsg())){echo $billetClique->getMsg();}else{echo 'ERROR Le message est vide ERROR';}?></textarea>
+        </label>
+        <p>L'auteur ne sera pas changer et la date sera mis à celle d'aujourd'hui.</p><br>
+        <input name="authorID" type="hidden" value="<?php echo $billetClique->getAuthorId()?>"/>
+        <input name="oldTitle" type="hidden" value="<?php echo $billetClique->getTitle()?>"/>
+
+        <input type="submit" name="BilletModif"/>
+    </section>
+</form>
+
+<p style="color: red"><?php if (isset($_SESSION['msg'])){echo $_SESSION['msg']; unset($_SESSION['msg']);}?></p>
 <?php
 end_page();
 ?>
