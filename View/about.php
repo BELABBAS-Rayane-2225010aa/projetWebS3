1dcbb7ae236c9281c5475305968fb3e4c3bdb84c
Crespin Alexandre <126071849+CRESPIN-Alexandre-2225022aa@users.noreply.github.com>
2023-11-05 18:06:38 +0100

    fix : about modifier

--- View/about.php
+++ View/about.php
@@ -9,9 +9,12 @@ start_page('Billet');//Charge la balise "head" avec le css, favicon et le nom de
 require 'HeaderMenu.php';//Charge le bar menu?>
 <section class="about">
     <label class="aboutlab">
-        <p> Ce site internet est réalisé pour un travail de groupe d'étudiant.<br>
+        <p>Ce site internet est réalisé pour un travail de groupe d'étudiant.<br>
         Ce site est libre de droit.<br>
-        Réaliser par: Belabbas,Créspin,De Angeli,Hourlay,Roubaud,Rousset<p>
+        <p>Réaliser par: Belabbas,Créspin,De Angeli,Hourlay,Roubaud,Rousset</p>
+        <p>Il s'agit d'un travail étudiant dans le cadre de notre 2ème année de BUT informatique<br>
+            à l'IUT d'Aix-En-Provence</p>
+        <p>C'est un projet open source : <a href="https://github.com/BELABBAS-Rayane-2225010aa/projetWebS3">voici notre github</a></p>
     </label><br>
 </section>
 <?php
